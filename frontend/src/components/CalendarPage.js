335a9d936c5e20212f3c3b3056edaa67a7cc1b07
Marina Massoud <marina.massoud@aalto.fi>
2021-10-18 22:55:02 +0300

    Add calendar library

--- frontend/src/components/CalendarPage.js
+++ frontend/src/components/CalendarPage.js
@@ -1,5 +1,28 @@
+import React from "react";
+import Calendar from "./Calendar";
+
+const mockEvents = [
+  {
+    title: "Maths 101 - DL 4",
+    start: Date.parse("18 Oct 2021 13:00:00 GMT"),
+    end: Date.parse("18 Oct 2021 15:00:00 GMT"),
+  },
+  {
+    title: "Programming 2 - Assignment B",
+    start: Date.parse("19 Oct 2021 12:00:00 GMT"),
+    end: Date.parse("19 Oct 2021 16:00:00 GMT"),
+  },
+];
+
 const CalendarPage = () => {
-  return <h1 data-testid="calendar-title">Calendar</h1>;
+  return (
+    <>
+      <h1 data-testid="calendar-title">Calendar</h1>
+      <div className="calendar-container">
+        <Calendar events={mockEvents} />
+      </div>
+    </>
+  );
 };
 
 export default CalendarPage;
