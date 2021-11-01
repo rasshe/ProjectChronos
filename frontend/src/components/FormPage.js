8844cdc649a20fec6aaf477ac656d1a3e6ce55b8
Marina Massoud <marina.massoud@aalto.fi>
2021-11-01 15:50:54 +0200

    Parse ical file and create deadlines

--- frontend/src/components/FormPage.js
+++ frontend/src/components/FormPage.js
@@ -1,14 +1,42 @@
 import React, { useState } from "react";
 import { Form, Row, Button, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import axiosInstance from "../axios";
+import { useHistory } from "react-router-dom";
 
 const FormPage = () => {
+  const history = useHistory();
   const [listHidden, setListHidden] = useState(true);
-  const handleSubmit = (e) => {
+  const [newDeadlines, setNewDeadlines] = useState([]);
+  const [file, setFile] = useState("");
+  const handleSubmitFile = (e) => {
     setListHidden(false);
     e.preventDefault();
-    axiosInstance.post("form/").then((r) => console.log(r.data));
+    if (!file) {
+      alert("No file selected");
+      return;
+    }
+    let formData = new FormData();
+    formData.append("file", file);
+    axiosInstance.post("calendar_file/", formData).then((r) => {
+      console.log(r.data);
+      setNewDeadlines(r.data);
+    });
+  };
+  const handleTimeAllocation = (value, index) => {
+    newDeadlines[index]["allocation"] = value;
+    setNewDeadlines(newDeadlines);
+  };
+  const handleSubmitDeadlines = () => {
+    const check = newDeadlines.every((dl) => dl.allocation);
+    if (!check) {
+      alert("Not all deadlines allocated");
+      return;
+    }
+    let formData = new FormData();
+    formData.append("data", JSON.stringify(newDeadlines));
+    axiosInstance.post("deadlines/", formData).then((r) => {
+      history.push("/calendar");
+    });
   };
   return (
     <>
@@ -18,13 +46,16 @@ const FormPage = () => {
         <Form>
           <Form.Group as={Row}>
             <Form.Label>Enter File</Form.Label>
-            <Form.Control type="file"></Form.Control>
+            <Form.Control
+              type="file"
+              onChange={(e) => setFile(e.target.files[0])}
+            ></Form.Control>
           </Form.Group>
           <Button
             as={Row}
             variant="success"
             type="submit"
-            onClick={handleSubmit}
+            onClick={handleSubmitFile}
           >
             {" "}
             Check{" "}
@@ -33,17 +64,24 @@ const FormPage = () => {
 
         <br />
 
-        <div hidden={listHidden}>
-          <h3>DL1: maths</h3>
-          Time allocation: <input type="number"></input>
-          <hr />
-          <h3>DL2: programming 1</h3>
-          Time allocation: <input type="number"></input>
-          <hr />
-          <Link class="btn btn-primary" to="/calendar">
-            {" "}
-            Allocate and submit{" "}
-          </Link>
+        <div hidden={newDeadlines.length === 0}>
+          {newDeadlines.map((dl, i) => (
+            <div key={i}>
+              <h3>{dl.summary}</h3>
+              <p>{dl.description}</p>
+              <p>
+                <b>DL:</b>
+                {dl.time}
+              </p>
+              Time allocation:{" "}
+              <input
+                type="number"
+                onChange={(e) => handleTimeAllocation(e.target.value, i)}
+              />
+              <hr />
+            </div>
+          ))}
+          <Button onClick={handleSubmitDeadlines}> Allocate and submit </Button>
         </div>
       </Container>
     </>
