0918bbb432a9ca38c3040a4456bed87f020f8e73
Ameni Mhadhbi <ameni.mhadhbi@aalto.fi>
2021-11-16 16:15:11 +0200

    Search bars

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j3d3qckc460m#%yw66=dzta0s57w$)7l+!mznbrb2h8o+yyi$4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
---
+++ frontend/src/components/EventCard.js
@@ -0,0 +1,62 @@
+import {
+    Carousel,
+    Card,
+    Col,
+    Row,
+    Button,
+    Container,
+    CardGroup,
+  } from "react-bootstrap";
+  import { useEffect, useReducer, useState } from "react";
+  import axiosInstance from "../axios";
+  
+  const images = [
+    "https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1576267423445-b2e0074d68a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1520881363902-a0ff4e722963?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  ];
+
+
+  const EventCard=(props)=>{
+
+      return(
+          <Card
+            style={{
+              margin: "5px",
+              minWidth: "24%",
+              maxWidth: "25%",
+              flex: "1 1 0px",
+            }}
+          >
+            <Card.Img
+              variant="top"
+              src={images[Math.floor(Math.random() * 3)]}
+            />
+            <Card.Body>
+              <Card.Title>{props.info.name}</Card.Title>
+              <Card.Text>
+                <b>Description:</b> <span style={{display: "-webkit-box", WebkitLineClamp: 3, WebkitBoxOrient: "vertical", overflow: "hidden"}}>{props.info.description}</span>
+                <br />
+                <b>Date and time:</b> {props.info.starting_time} to {props.info.end_time}
+                <br />
+                <b>Location:</b> {props.info.place}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <small className="text-muted">
+                {props.info.attendees} attendees!
+              </small>
+              <Button>Join</Button>
+            </Card.Footer>
+          </Card>
+
+      )
+  }
+
+  export default EventCard
